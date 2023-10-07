aa777a79e5d1f3f205777d487aab58a4c9acd17c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-07 23:53:23 +0900

    円の描画

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-'use strict'
-
+'use strict';
     (() => {
 
         class Icon {
             constructor(canvas) {
+                this.ctx = canvas.getContext('2d');
+                this.width = canvas.width;
+                this.height = canvas.height;
+                this.r = 60;
+            }
 
+            draw() {
+                this.ctx.beginPath();
+                this.ctx.arc(this.width / 2, this.height / 2, this.r, 0, 2 * Math.PI);
+                this.ctx.stroke();
             }
 
             run() {
-
+                this.draw();
             }
         }
-        
+
         const canvas = document.querySelector('canvas');
         if (typeof canvas.getContext === 'undefined') {
             return;
