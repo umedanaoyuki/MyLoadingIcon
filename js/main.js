8681b672f10e1373f524e63a6251a61f38501845
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-07 23:45:44 +0900

    雛形作成

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+    (() => {
+
+        class Icon {
+            constructor(canvas) {
+
+            }
+
+            run() {
+
+            }
+        }
+        
+        const canvas = document.querySelector('canvas');
+        if (typeof canvas.getContext === 'undefined') {
+            return;
+        }
+
+        const icon = new Icon(canvas);
+        icon.run();
+
+    })();
